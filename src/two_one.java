3b7e13d7ff5794b514470f438e7d6a56ddd10ab0
Админ <dimashunk_o@mail.ru>
2021-02-22 21:38:06 +0300

    Commit 1

---
+++ src/two_one.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class two_one {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = sc.nextInt();
+        System.out.println("Введите число b");
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println("Результат деления:" +a/b);
+        System.out.println("Остаток от деления:" +a%b);
+    }
+}
